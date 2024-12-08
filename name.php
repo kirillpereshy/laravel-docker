e83cacc5f42fd187095f7892a5db66a52ce0b0a6
kirill <19830403x@gmail.com>
2024-12-08 12:32:56 +0200

    new data

--- name.php
+++ name.php
@@ -1,3 +1,9 @@
 <?php
-    echo 'master';
+    $number = 1; {
+        if ($number == 10) {
+            echo 'меньше 10';
+        } elseif ($number < 20) {
+            echo 'больше 10';
+        }
+}
 ?>
